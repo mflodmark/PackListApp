b89829ec04a2272ae5bf6a76655c75f98c50129f
Markus Flodmark <markus.flodmark@gmail.com>
2018-02-26 19:07:49 +0100

    rename

--- PackListApp/PackListApp/NewQuickie.xaml.cs
+++ PackListApp/PackListApp/NewQuickie.xaml.cs
@@ -1,49 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PackListApp.Models;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PackListApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewQuickie : ContentPage
-	{
-	    private readonly ObservableCollection<Quickies> _quickies;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OptionQuickie : ContentPage
+    {
+        public OptionQuickie()
+        {
+            InitializeComponent();
+        }
 
-	    public NewQuickie (ObservableCollection<Quickies> quickies)
-		{
-			InitializeComponent ();
+        private void SunBtn_OnClicked(object sender, EventArgs e)
+        {
 
-		    _quickies = quickies;
-		}
-
-	    private void SunBtn_OnClicked(object sender, EventArgs e)
-	    {
             PushToList();
-	    }
+        }
+
+        private void SnowBtn_OnClicked(object sender, EventArgs e)
+        {
+
+            PushToList();
+        }
+
 
 
         private async void PushToList()
-	    {
-	        try
-	        {
-	            await Navigation.PushAsync(new ListQuickie(_quickies));
-            }
-            catch (Exception)
-	        {
-	            await DisplayAlert("Error", "Can't access page", "Ok");
-	        }
+        {
+            await Navigation.PushAsync(new ListQuickie());
         }
-
-	    private void Snow_OnTapped(object sender, EventArgs e)
-	    {
-	        PushToList();
-	    }
-
-	}
+    }
 }
